74076a1ba291732add03f282cc0239bab906686a
Samriddha Pathak <samriddhapathak123333@gmail.com>
2025-01-20 23:28:19 +0545

    Update congesture.py

--- Collatz_Conjecture/congesture.py
+++ Collatz_Conjecture/congesture.py
@@ -1,19 +1,31 @@
 import csv
 
+# Ask the user to enter a number 'n'.
 n = int(input("Enter a number: "))
 
+# Open a file named 'collatz_conjecture.csv' for writing.
 with open('collatz_conjecture.csv', 'w', newline='') as file:
+    # Create a writer to write data to the CSV file.
     writer = csv.writer(file)
+
+    # Write the column names ('number' and 'collatz_conjecture') at the top of the CSV file.
     writer.writerow(['number', 'collatz_conjecture'])
 
-    for i in range(1, n + 1, 1):
-        con_list = [i]
-        original_i = i  # Save the original number
-        while i != 1:
-            if i % 2 == 0:
-                i = i // 2  # Integer division to keep i as an integer
+    # Go through each number from 1 to 'n'.
+    for num in range(1, n + 1):
+        # Start the Collatz sequence with the current number.
+        collatz_sequence = [num]
+        
+        # Keep going until the number becomes 1.
+        while num != 1:
+            # If the number is even, divide it by 2.
+            if num % 2 == 0:
+                num = num // 2
+            # If the number is odd, multiply it by 3 and add 1.
             else:
-                i = i * 3 + 1
-            con_list.append(i)
-        writer.writerow([original_i, con_list])
-        con_list.clear()
+                num = num * 3 + 1
+            # Add the new number to the Collatz sequence.
+            collatz_sequence.append(num)
+        
+        # Write the original number and its Collatz sequence to the file.
+        writer.writerow([collatz_sequence[0], collatz_sequence])
